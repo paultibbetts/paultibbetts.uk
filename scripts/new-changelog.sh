c8a4c5b8d6629f71edef0b0bd05e17a2a2f746f7
Paul Tibbetts <code@paultibbetts.uk>
2026-09-21 16:01:00 +0100

    chore: change new changelog script to show example of datetime

--- scripts/new-changelog.sh
+++ scripts/new-changelog.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+usage() {
+  echo "Usage:"
+  echo "  $0"
+  echo "  $0 \"2026-03-17T14:14:56Z\""
+}
+
 if [[ $# -gt 1 ]]; then
-  echo "Usage: $0 [optional-ISO-8601-datetime]"
+  usage
   exit 1
 fi
 
@@ -11,6 +17,7 @@ datetime="${1:-}"
 if [[ -n "$datetime" ]]; then
   if [[ ! "$datetime" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}T ]]; then
     echo "Error: datetime must begin with an ISO-8601 date and time."
+    usage
     exit 1
   fi
   date_part="${datetime:0:10}"
@@ -22,6 +29,7 @@ file="content/changelog/${date_part}.md"
 
 if [[ -e "$file" ]]; then
   echo "Error: A changelog entry already exists for $date_part."
+  usage
   exit 1
 fi
 
